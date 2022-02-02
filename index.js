6eeb21f9b4b18a4cd380f879034ba0327b9d68be
Ricky Jones <ricky.jones300@gmail.com>
2022-02-02 22:21:17 +0900

    Completed exercises

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function addUp(number) {
-  return Array.from(new Array(number)).map((val,i) => ++i).reduce((acc, cur) => acc + cur);
+    return Array.from(new Array(number)).map((val,i) => ++i).reduce((acc, cur) => acc + cur);
 }
 
 /* 
@@ -8,21 +8,32 @@ function addUp(number) {
   If the input is empty, return an empty string "".
 */
 function singleOccurance(str) {
-  
+    let count = {};
+    for(let c of str.toUpperCase()) {
+        count[c] = c in count ? count[c] + 1 : 1;
+    }
+    for(let c in count) {
+        if (count[c] === 1) return c;
+    }
+    return '';
 }
 
 /* 
   Create a function that takes a string and returns a string in which each character is repeated once.
 */
 function doubleChar(str) {
-
+    let result = '';
+    for(let c of str) {
+        result += c + c;
+    }
+    return result;
 }
 
 /* 
   Create a function that returns an array of strings sorted by length in ascending order.
 */
 function sortByLength(arr) {
-
+    return arr.sort((a, b) => a.length - b.length);
 }
 
 /* 
@@ -31,7 +42,7 @@ function sortByLength(arr) {
   You will always be supplied with at least one parameter.
 */
 function allTruthy(...arr) {
-
+    return arr.every(x => Boolean(x));
 }
 
 /* 
@@ -39,7 +50,7 @@ function allTruthy(...arr) {
   There will be at least two numbers in the array.
 */
 function secondLargest(arr) {
-
+    return arr.sort((a, b) => a - b).at(-2);
 }
 
 /* 
@@ -47,14 +58,18 @@ function secondLargest(arr) {
   Return an empty array if the object is empty.
 */
 function toArray(obj) {
-
+    let result = [];
+    for(let k in obj) result.push([k, obj[k]]);
+    return result;
 }
 
 /* 
   Create a function which takes in a word and spells it out, by consecutively adding letters until the full word is completed.
 */
 function spelling(str) {
-
+    let result = [];
+    for(let i = 1; i <= str.length; i++) result.push(str.slice(0, i));
+    return result;
 }
 
 /* 
@@ -69,7 +84,12 @@ function spelling(str) {
   "user1, user2 and 3 more online"
 */
 function chatroomStatus(arr) {
-
+    switch(arr.length) {
+        case 0: return 'no one online';
+        case 1: return `${arr[0]} online`;
+        case 2: return `${arr[0]} and ${arr[1]} online`;
+        default: return `${arr[0]}, ${arr[1]} and ${arr.length - 2} more online`;
+    }
 }
 
 /* 
@@ -77,7 +97,7 @@ function chatroomStatus(arr) {
   A single printed page requires each color once, so printing is not possible if any of the slots lack ink
 */
 function inkLevels(obj) {
-
+    return Math.min.apply(null, Object.values(obj));
 }
 
 /* 
@@ -88,14 +108,16 @@ function inkLevels(obj) {
   You may get a number of minutes over 99 (e.g. "121:49" is perfectly valid).
 */
 function minutesToSeconds(str) {
-
+    let [minutes, seconds] = str.split(':');
+    if(Number(seconds) >= 60) return false;
+    return Number(minutes) * 60 + Number(seconds);
 }
 
 /* 
   Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
 */
 function reverseCase(str) {
-
+    return str.split('').map(c => c.toUpperCase() === c ? c.toLowerCase() : c.toUpperCase()).join('');
 }
 
 /* 
@@ -103,14 +125,19 @@ function reverseCase(str) {
   A number is symmetrical when it is the same as its reverse.
 */
 function isSymmetrical(num) {
-
+    let str = num.toString();
+    for(let i = 0; i < str.length / 2; i++) {
+        if(str[i] !== str.at(-(1+i))) return false;
+    }
+    return true;
 }
 
 /* 
   Create a function to return the amount of potatoes there are in a string.
 */
 function potatoes(str) {
-
+    let matches = str.match(/potato/g);
+    return matches === null ? 0 : matches.length;
 }
 
 /* 
@@ -118,7 +145,7 @@ function potatoes(str) {
   Your output must be case-sensitive (see second test).
 */
 function charCount(char, str) {
-
+    return str.split('').filter(c => c === char).length;
 }
 
 /* 
@@ -127,7 +154,7 @@ function charCount(char, str) {
   You can expect non-negative numbers for all test cases.
 */
 function sortDescending(num) {
-
+    return Number(num.toString().split('').sort().reverse().join(''));
 }
 
 module.exports = { reverseCase, sortDescending, charCount, potatoes, singleOccurance, addUp, doubleChar, sortByLength, allTruthy, secondLargest, toArray, spelling, chatroomStatus, inkLevels, isSymmetrical, minutesToSeconds };
